492d1065b68e519cf637060fbcebfc26af2601f8
Olivier Sohn <olivier.sohn@gmail.com>
2017-04-23 00:30:06 +0200

    dev

--- source/main.join_non_zeros.cpp
+++ source/main.join_non_zeros.cpp
@@ -2,64 +2,19 @@
 
 namespace imajuscule {
     namespace audio {
-        template<int SAMPLE_SIZE>
-        void join_non_zeros(DirectoryPath const & dir, FileName const & filename, WAVReader & reader) {
-            using namespace std;
-            using namespace imajuscule;
-            using namespace imajuscule::audio;
-
-            auto n_channels = reader.countChannels();
-            auto n_frames = reader.countFrames();
-            
-            using T = typename SignedTypeForSampleSize<SAMPLE_SIZE>::type;
-            static_assert(sizeof(T) == SAMPLE_SIZE, "");
-            
-            std::vector<T> buf(n_frames*n_channels);
-            
-            {
-                auto end = reader.Read(buf.begin(), buf.end());
-                A(end == buf.end());
-                A(!reader.HasMore());
-            }
-            
-            std::vector<T> buf_nozero;
-            
-            auto it = buf.begin();
-            auto end = buf.end();
-            while(it != end) {
-                bool ok = false;
-                for(int j=0; j<n_channels; j++) {
-                    if(0 != *(it+j)) {
-                        ok = true;
-                        break;
+        
+        static inline void join_non_zeros(DirectoryPath const & dir, FileName const & filename) {
+            filter_frames(dir,
+                          filename,
+                          "joined_" + filename,
+                          [](auto & frame) {
+                for(auto c : frame) {
+                    if(0 != c) {
+                        return true;
                     }
                 }
-                if(ok) {
-                    for(int j=0; j<n_channels; j++) {
-                        buf_nozero.push_back(*(it+j));
-                    }
-                }
-                it += n_channels;
-            }
-            
-
-            auto header = pcm(reader.getFormat(),
-                              buf_nozero.size() / n_channels,
-                              reader.getSampleRate(),
-                              NChannels(n_channels),
-                              AudioSample<T>::format);
-            
-            WAVWriter writer(dir, "joined_" + filename, header);
-            auto res = writer.Initialize();
-            
-            if(ILE_SUCCESS != res) {
-                cerr << "could not create file" << endl;
-                throw;
-            }
-            
-            for(auto c : buf_nozero) {
-                writer.writeSample(c);
-            }
+                return false;
+            } );
         }
     }
 }
@@ -87,30 +42,8 @@ int main(int argc, const char * argv[]) {
         cerr << "invalid path : " << argv[1] << endl;
         throw;
     }
-    
-    WAVReader reader(dir, filename);
-    
-    auto res = reader.Initialize();
-    
-    if(ILE_SUCCESS != res) {
-        cerr << "error opening file for read" << endl;
-        throw;
-    }
-    
-    switch(reader.getSampleSize()) {
-        case 2:
-            join_non_zeros<2>(dir, filename, reader);
-            break;
-        case 3:
-            join_non_zeros<3>(dir, filename, reader);
-            break;
-        case 4:
-            join_non_zeros<4>(dir, filename, reader);
-            break;
-        default:
-            throw;
-    }
-    
+   
+    join_non_zeros(dir, filename);
 
     return 0;
 }
