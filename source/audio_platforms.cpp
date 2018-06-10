b8203520112abc30db7ecf86ac4d804655f17390
Olivier Sohn <olivier.sohn@gmail.com>
2018-06-10 16:14:18 +0200

    Denormals

--- source/audio_platforms.cpp
+++ source/audio_platforms.cpp
@@ -6,17 +6,17 @@ namespace imajuscule {
         * Denormals can appear in reverb algorithm, when signal becomes close to 0.
         */
         void disableDenormals() {
-          #if __APPLE__
-            fesetenv(FE_DFL_DISABLE_SSE_DENORMS_ENV);
-#if TARGET_OS_IOS
-            fesetenv(FE_DFL_DISABLE_DENORMS_ENV);
+#if __has_include(<fenv.h>)
+          fesetenv(FE_DFL_DISABLE_SSE_DENORMS_ENV);
+#  if TARGET_OS_IOS
+          fesetenv(FE_DFL_DISABLE_DENORMS_ENV);
+#  endif
+#else
+#  define CSR_FLUSH_TO_ZERO         (1 << 15)
+          unsigned csr = __builtin_ia32_stmxcsr();
+          csr |= CSR_FLUSH_TO_ZERO;
+          __builtin_ia32_ldmxcsr(csr);
 #endif
-          #else
-          #define CSR_FLUSH_TO_ZERO         (1 << 15)
-              unsigned csr = __builtin_ia32_stmxcsr();
-              csr |= CSR_FLUSH_TO_ZERO;
-              __builtin_ia32_ldmxcsr(csr);
-          #endif
         }
 
         // no need to synchronize access to this : it's 4 bytes-aligned,
--- source/private.h
+++ source/private.h
@@ -1,7 +1,8 @@
 #include "../include/public.h"
 
-#if __APPLE__
-#include <fenv.h>
+#if __has_include(<fenv.h>)
+#  include <fenv.h>
+#endif
 
 #if TARGET_OS_IOS
 // on ios, these macros are hidden from fenv.h:
@@ -13,8 +14,6 @@ extern const fenv_t _FE_DFL_DISABLE_DENORMS_ENV;
 // from https://github.com/mstg/iOS-full-sdk/blob/master/iPhoneOS9.3.sdk/usr/include/fenv.h
 extern const fenv_t _FE_DFL_DISABLE_SSE_DENORMS_ENV;
 #define FE_DFL_DISABLE_SSE_DENORMS_ENV  &_FE_DFL_DISABLE_SSE_DENORMS_ENV
-
-#endif
 
 #endif
 
